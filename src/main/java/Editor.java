2c5261c3799eae1ee52632034fbe692a17430ce0
Taylor Allen <taylorallen0913@gmail.com>
2020-05-18 15:12:47 -0700

    first commit

---
+++ src/main/java/Editor.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.BorderLayout;
+import org.fife.ui.rtextarea.*;
+import org.fife.ui.rsyntaxtextarea.*;
+
+public class Editor extends JFrame {
+    public Editor() {
+
+        JPanel cp = new JPanel(new BorderLayout());
+
+        RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+        textArea.setCodeFoldingEnabled(true);
+        RTextScrollPane sp = new RTextScrollPane(textArea);
+        cp.add(sp);
+
+        setContentPane(cp);
+        setTitle("Text Editor Demo");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+
+    }
+
+    public static void main(String[] args) {
+        // Start all Swing applications on the EDT.
+        SwingUtilities.invokeLater(() -> {
+            new Editor().setVisible(true);
+        });
+    }
+}
---
+++ src/main/java/LaunchMenu.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class LaunchMenu {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
